57a611df8536e185c29ed903fa4959f2da856f29
이석훈 <lswjjhlsh49@gmail.com>
2023-05-25 15:09:17 +0000

    123

--- js/search.js
+++ js/search.js
@@ -1,11 +1,15 @@
 document.getElementById("search_btn").addEventListener('click', search_message);
 
 var search_array =[] //빈 배열- 전역 변수
-var MAX_SEARCH_COUNT = 3;
+var MAX_SEARCH_COUNT = 10;
 
 function search_message(){
 
 let search_str = document.querySelector("#search_txt"); // 변수에 저장
+	    let non_searchwords =["바보", "씨발","시발","새끼"]
+    if(non_searchwords.some(word => search_str.value.includes(word))) { // 제한 단어 확인
+      alert("해당 단어는 검색할 수 없습니다."); // 제한 단어 알림
+   }
 if(search_str.value === 0){ 
        alert("검색어가 비었습니다. 입력해주세요"); 
     }
